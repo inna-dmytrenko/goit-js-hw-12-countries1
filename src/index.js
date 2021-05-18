c2e41ca77796d9cac9b417e3b05342484d8b54af
inna-dmytrenko <73888831+inna-dmytrenko@users.noreply.github.com>
2021-05-18 08:23:39 +0300

    1

---
+++ src/index.js
@@ -0,0 +1,66 @@
+import './styles.css';
+import fetchCountries from './js/fetchCountries';
+import countryListItemsTemplate from './templates/country.hbs';
+import countriesListTemplate from './templates/countries.hbs';
+import PNotify from 'pnotify/dist/es/PNotify.js';
+import PNotifyStyleMaterial from 'pnotify/dist/es/PNotifyStyleMaterial.js';
+import debounce from 'lodash.debounce';
+
+const refs = {
+  form: document.querySelector('#search-form'),
+  list: document.querySelector('#country-list'),
+  // input: document.querySelector('.search__input'),
+};
+
+refs.form.addEventListener('submit', event => {
+  event.preventDefault();
+});
+
+refs.form.addEventListener(
+  'input',
+  debounce(e => {
+    searchFormInputHandler(e);
+  }, 500),
+);
+
+function searchFormInputHandler(e) {
+  const searchQuery = e.target.value;
+
+  clearListItems();
+
+  fetchCountries(searchQuery).then(data => {
+    const markup = buildListItemMarkup(data);
+    const renderCountriesList = buildCountriesList(data);
+    if (!data) {
+      return;
+    } else if (data.length > 10) {
+      PNotify.defaults.styling = 'material';
+      PNotify.error({
+        
+        text: 'Too many matches found.Please enter a more specific query',
+      });
+    } else if (data.length >= 2 && data.length <= 10) {
+      insertListItem(renderCountriesList);
+    } else if (data.length === 1) {
+      insertListItem(markup);
+    } else {
+      alert('Ничего не найдено.Корректно введите запрос');
+    }
+  });
+}
+
+function insertListItem(items) {
+  refs.list.insertAdjacentHTML('beforeend', items);
+}
+
+function buildCountriesList(items) {
+  return countriesListTemplate(items);
+}
+
+function buildListItemMarkup(items) {
+  return countryListItemsTemplate(items);
+}
+
+function clearListItems() {
+  refs.list.innerHTML = '';
+}
